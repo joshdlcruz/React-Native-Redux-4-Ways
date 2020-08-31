b1d6b06bd26650baab16405899e5d1bfa16be940
joshdlcruz <dcjoshua25@gmail.com>
2020-08-31 10:30:45 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-Native-Redux-4-Ways
---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
+import App from './App';
+
+const store = configureStore();
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
